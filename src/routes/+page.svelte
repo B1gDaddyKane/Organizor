8a2050d7b7e6db633167da5f1bade7c8ccd3c56c
B1gDaddyKane <jenschrj97@gmail.com>
2023-01-06 22:21:35 +0100

    Added a function for generating all timeslots for a day and for generating timeslots that should be generated to a list to display

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,58 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	let timeSlots: string[] = [];
+	let taskContainers: string[] = [];
+	let startTime: string;
+	let endTime: string;
+
+	const generateTimeslots = () => {};
+
+	const getTimes = (start: string, finish: string): string[] => {
+		let startDateTime = new Date('1970-01-01T' + start + 'Z');
+		let finishDateTime = new Date('1970-01-01T' + finish + 'Z');
+
+		let arr: string[] = [];
+		arr.push(start);
+
+		while (startDateTime < finishDateTime) {
+			startDateTime.setMinutes(startDateTime.getMinutes() + 30);
+
+			arr.push(startDateTime.toISOString().substr(11, 5));
+		}
+
+		return arr;
+	};
+
+	$: startTime && endTime ? (taskContainers = getTimes(startTime, endTime)) : null;
+
+	onMount(() => {
+		timeSlots = getTimes('00:30', '24:00');
+	});
+</script>
+
+<div class="text-center m-2">
+	<div class="">
+		<select bind:value={startTime}>
+			{#each timeSlots as timeSlot}
+				<option value={timeSlot}>{timeSlot}</option>
+			{/each}
+		</select>
+		<select bind:value={endTime}>
+			{#each timeSlots as timeSlot}
+				<option value={timeSlot}>{timeSlot}</option>
+			{/each}
+		</select>
+	</div>
+</div>
+<div class="grid grid-flow-col">
+	{#each taskContainers as taskContainer}
+		<div>
+			<div class="text-center pt-4 border-b-2">
+				<p class="border-r-2">
+					{taskContainer}
+				</p>
+			</div>
+		</div>
+	{/each}
+</div>
