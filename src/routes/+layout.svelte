7d2231ee1b6df7101500f856f917317a651c482e
B1gDaddyKane <jenschrj97@gmail.com>
2023-01-08 20:56:03 +0100

    Added navbar and drag/drop functions that changes the color of the drop zone to the color of the draggable object

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,5 @@
 	import '../app.css';
 </script>
 
+<nav class="sticky top-0 text-center m-2"><a href="/">Home</a></nav>
 <slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,10 +3,20 @@
 
 	let timeSlots: string[] = [];
 	let taskContainers: string[] = [];
-	let startTime: string;
-	let endTime: string;
+	let startTime: string = '06:00';
+	let endTime: string = '21:00';
+	let color: string;
 
-	const generateTimeslots = () => {};
+	const onDrag = (e: DragEvent) => {
+		(e.target as HTMLDivElement).classList.forEach((colorClass) => {
+			if (colorClass.includes('bg-')) color = colorClass;
+		});
+	};
+
+	const onDrop = (e: DragEvent) => {
+		e.preventDefault();
+		(e.target as HTMLDivElement).setAttribute('class', `w-full p-2 ${color}`);
+	};
 
 	const getTimes = (start: string, finish: string): string[] => {
 		let startDateTime = new Date('1970-01-01T' + start + 'Z');
@@ -18,7 +28,7 @@
 		while (startDateTime < finishDateTime) {
 			startDateTime.setMinutes(startDateTime.getMinutes() + 30);
 
-			arr.push(startDateTime.toISOString().substr(11, 5));
+			arr.push(startDateTime.toISOString().substring(11, 16));
 		}
 
 		return arr;
@@ -31,28 +41,46 @@
 	});
 </script>
 
-<div class="text-center m-2">
-	<div class="">
-		<select bind:value={startTime}>
-			{#each timeSlots as timeSlot}
-				<option value={timeSlot}>{timeSlot}</option>
-			{/each}
-		</select>
-		<select bind:value={endTime}>
-			{#each timeSlots as timeSlot}
-				<option value={timeSlot}>{timeSlot}</option>
-			{/each}
-		</select>
+<div class="grid grid-cols-2">
+	<div class="grid-flow-row">
+		{#each taskContainers as taskContainer}
+			<div class="grid grid-cols-2 border-b-2 border-b-white">
+				<div class="text-center">
+					<p>
+						{taskContainer}
+					</p>
+				</div>
+				<div
+					on:drop={onDrop}
+					on:dragover={(e) => e.preventDefault()}
+					on:dragenter={(e) => e.preventDefault()}
+					class="w-full bg-slate-400 p-2"
+				/>
+			</div>
+		{/each}
 	</div>
-</div>
-<div class="grid grid-flow-col">
-	{#each taskContainers as taskContainer}
-		<div>
-			<div class="text-center pt-4 border-b-2">
-				<p class="border-r-2">
-					{taskContainer}
-				</p>
+	<div>
+		<div class="text-center m-2">
+			<div class="">
+				<select class="border-2 border-blue-400 rounded-lg" bind:value={startTime}>
+					<option value="" disabled selected>Start tid</option>
+					{#each timeSlots as timeSlot}
+						<option value={timeSlot}>{timeSlot}</option>
+					{/each}
+				</select>
+				<select class="border-2 border-blue-400 rounded-lg" bind:value={endTime}>
+					<option value="" disabled selected>Slut tid</option>
+					{#each timeSlots as timeSlot}
+						<option value={timeSlot}>{timeSlot}</option>
+					{/each}
+				</select>
+			</div>
+			<div class="grid grid-cols-3 m-2 justify-items-center">
+				<div on:drag={onDrag} draggable="true" class="bg-blue-500 w-2/4 p-2 mb-2" />
+				<div on:drag={onDrag} draggable="true" class="bg-green-500 w-2/4 p-2 mb-2" />
+				<div on:drag={onDrag} draggable="true" class="bg-yellow-500 w-2/4 p-2 mb-2" />
+				<div on:drag={onDrag} draggable="true" class="bg-red-500 w-2/4 p-2 mb-2" />
 			</div>
 		</div>
-	{/each}
+	</div>
 </div>
